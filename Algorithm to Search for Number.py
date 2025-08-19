1381a7da4ea510e8c593e0406303099780b198fc
Brahnavan-lab <brahaselva@gmail.com>
2025-08-19 16:10:39 +0100

    Add files via upload

--- Algorithm to Search for Number.py
+++ Algorithm to Search for Number.py
@@ -1,14 +1,17 @@
 list1 = [5,14,3,19,28]
 search = int(input("Enter a search item"))
+found = False
 count = 0
 for i in range(len(list1)):
     if list1[count] == search:
         print("Well done. The search item has been found. It is in position",count+1)
-    elif list1[count] != search:
-        print("This is not search item. Try again")
-        count = count+1
+        found = True 
+        break
+    count = count+1
 else:
     print("You have reached the end of the list. The value is not in the list")
+
+
     
 
 
